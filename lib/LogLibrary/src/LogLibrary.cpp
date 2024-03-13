2136126f1329f2e4af34dd1413fe75c418d0ea94
Rubens05 <rubenggjac@hotmail.com>
2024-03-13 19:13:43 +0100

    Primera implementación de librería Arduino, estructura de directorios hecha, librería en local en el propio proyecto

---
+++ lib/LogLibrary/src/LogLibrary.cpp
@@ -0,0 +1,8 @@
+#include "LogLibrary.h"
+
+void function()
+{
+    // put your main code here, to run repeatedly:
+    Serial.println("Hello World from function!");
+    delay(1000);
+}
---
+++ lib/LogLibrary/src/LogLibrary.h
@@ -0,0 +1,9 @@
+#ifndef LOGLIBRARY_H
+#define LOGLIBRARY_H
+#include <Arduino.h>
+// Definir las funciones que se van a utilizar
+
+void function();
+
+//
+#endif
