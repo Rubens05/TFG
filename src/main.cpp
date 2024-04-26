a4c382fc9842d6114ad3b582ed0d4752e496a902
Rubens05 <rubenggjac@hotmail.com>
2024-04-26 15:53:39 +0200

    Added authorization to MQTT config file

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,43 @@
 // const char *ssid = "Orange-E475";
 // const char *password = "3mcUAJZC";
 // const char *mqttServer = "192.168.1.117";
-
-// // const char *ssid = "DIGIFIBRA-HdQF";
-// // const char *password = "4FXX5tAc37bA";
-// // const char *mqttServer = "192.168.1.131";
 // const int mqttPort = 1883;
-// const char *mqttTopic = "logStatus";
-// const char *idBoard = "PlacaXD";
-// const char *timeZone = "CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00"; // https://remotemonitoringsystems.ca/time-zone-abbreviations.php
-// const char *ntpServer = "pool.ntp.org";                                // https://www.ntppool.org/es/zone/es
-// const char *logFormat = "{level}-{message}-{timestamp}-{idSender}";
+// const char *mqttUser = "user";
+// const char *mqttPassword = "user1";
+// const char *mqttTopic = "mqttTopic";
+// const char *idBoard = "idBoard";
+
+// // Change TimeZone https://remotemonitoringsystems.ca/time-zone-abbreviations.php
+// // Default timezone is Madrid
+// // const char *timeZone = "CET-1CEST-2,M3.5.0/02:00:00,M10.5.0/03:00:00";
+
+// // Change ntpServer https://www.ntppool.org/es/zone/es
+// // Default ntpServer is pool.ntp.org
+// // const char *ntpServer = "pool.ntp.org";
+
+// // Change log format optionally
+// // Default format: "{level}-{message}-{idSender}-{topic}-{timestamp}"
+// // const char *logFormat2 = "{message} - {timestamp} - {idSender}";
+// // const char *logFormat1 = "{level} - {message}";
+// // const char *logFormat3 = "{level} - {message} - {idSender}";
+
 // Logger logger;
 
-// void funcionalidadUsuario();
+// void userFunctionality();
 
 // void setup()
 // {
 //     Serial.begin(9600);
+
+//     // Set log format
+//     // logger.setLogFormat(logFormat2);
+//     // logger.setLogFormat(logFormat3);
+//     // logger.setLogFormat(logFormat4);
+
+//     // Send logs to MQTT in order to be stored in a database
+//     // logger.sendToMQTT(mqttUser, mqttPassword);
+
+//     // Initialize logger with different configurations
 //     logger.init(ssid, password, mqttServer, mqttPort);
 //     // logger.init(ssid, password, mqttServer, mqttPort, mqttTopic);
 //     // logger.init(ssid, password, mqttServer, mqttPort, mqttTopic, idBoard);
@@ -30,44 +50,39 @@
 
 // void loop()
 // {
-//     // Generar un número aleatorio entre 0 y 8
-//     int numeroAleatorio = random(0, 4);
-//     // FUNCIONALIDAD DEL USUARIO
-//     funcionalidadUsuario();
+//     // Random number between 0 and 25
+//     int number = random(0, 26);
 
-//     switch (numeroAleatorio)
+//     // User functionality
+//     userFunctionality();
+
+//     if (number >= 0 && number <= 5)
 //     {
-//     // EL USUARIO DECIDE CUANDO LLAMAR AL METODO DE LOG
-//     case 0:
-//         Serial.print("INFO");
-
-//         logger.logINFO("Mensaje de info!");
-//         break;
-//     case 1:
-//         Serial.print("WARNING");
-
-//         logger.logWARNING("Mensaje de warning!");
-//         break;
-//     case 2:
-//         Serial.print("ERROR");
-
-//         logger.logERROR("Mensaje de error!");
-//         break;
-//     case 3:
-//         Serial.print("CRITICO");
-
-//         logger.logCRITICO("Mensaje crítico!");
-//         break;
-//     default:
-//         break;
+//         logger.logDEBUG("Debug in the user's board");
+//     }
+//     else if (number >= 6 && number <= 10)
+//     {
+//         logger.logWARNING("Warning in the user's board");
+//     }
+//     else if (number >= 11 && number <= 15)
+//     {
+//         logger.logERROR("Error in the user's board");
+//     }
+//     else if (number >= 16 && number <= 20)
+//     {
+//         logger.logINFO("Information in the user's board");
+//     }
+//     else if (number >= 21 && number <= 25)
+//     {
+//         logger.logCRITICAL("Critical in the user's board");
 //     }
 // }
 
-// void funcionalidadUsuario()
+// void userFunctionality()
 // {
-//     // Funcion cualquiera de la placa del usuario
-//     // No se debe interrumpir su funcionamiento
-//     Serial.println(" ");
+//     // Functionality of the user's board
+//     // Its operation should not be interrupted
+//     Serial.println("User functionality running...");
 //     delay(500);
 // }
 
@@ -78,6 +93,8 @@ const char *ssid = "Orange-E475";
 const char *password = "3mcUAJZC";
 const char *mqttServer = "192.168.1.117";
 const int mqttPort = 1883;
+const char *mqttUser = "user";
+const char *mqttPassword = "user1";
 
 const char *mqttTopic = "logStatus";
 const char *idBoard = "PlacaXD";
@@ -110,7 +127,15 @@ void setup()
     // logger.setLogFormat(logFormat2);
     // logger.setLogFormat(logFormat3);
 
-    logger.init(ssid, password, mqttServer, mqttPort, mqttTopic, idBoard, timeZone, ntpServer);
+    // Para enviar los logs a MQTT y almacenarlos en una base de datos
+    logger.sendToMQTT(mqttUser, mqttPassword);
+
+    // Inicializar el logger con diferentes configuraciones
+    logger.init(ssid, password, mqttServer, mqttPort);
+    // logger.init(ssid, password, mqttServer, mqttPort, mqttTopic);
+    // logger.init(ssid, password, mqttServer, mqttPort, mqttTopic, idBoard);
+    // logger.init(ssid, password, mqttServer, mqttPort, mqttTopic, idBoard, timeZone);
+    // logger.init(ssid, password, mqttServer, mqttPort, mqttTopic, idBoard, timeZone, ntpServer);
 }
 
 void loop()
